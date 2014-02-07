43e670046e8d436346a398da1b54e3e3f388cf93
Adam Norwood <adam@adamnorwood.com>
2014-02-06 19:58:16 -0600

    Adds styling for Books archive page, works responsively

--- archive-single-books.php
+++ archive-single-books.php
@@ -9,17 +9,17 @@
         <?php if ( $author = get_field( 'author' ) ) : ?>
             <p class="book-meta-author"><?= $author ?></p>
         <?php endif; ?>
+        <a href="<?php the_permalink() ?>" class="read-more">Read more<span> about <?php the_title(); ?></span></a>
 
         <?php if ( have_rows( 'stores' ) ) : ?>
-            <div class="book-meta-stores">Buy now!
+            <div class="book-meta-stores">
+                <strong>Buy now!</strong>
                 <ul class="book-meta-stores-list">
                     <?php while ( have_rows( 'stores' ) ) : the_row(); ?>
-                    <li class="book-meta-store"><a href="<?php the_sub_field( 'store-url' ); ?>"><?php the_sub_field( 'store-name' ); ?></a></li>
+                    <li class="book-meta-store"><a class="button" href="<?php the_sub_field( 'store-url' ); ?>"><?php the_sub_field( 'store-name' ); ?></a></li>
                     <?php endwhile; ?>
                 </ul>
             </div>
         <?php endif; ?>
     </section>
-
-    <a href="<?php the_permalink() ?>" class="read-more">Read more <span>about <?php the_title(); ?></span></a>
 </article>
