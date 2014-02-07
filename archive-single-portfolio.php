b11851e847d1edbfaa234311c4cb740a5c65c8a6
Adam Norwood <adam@adamnorwood.com>
2014-02-07 16:00:11 -0600

    Updates portfolio thumbnail to drop HTML width and height properties

--- archive-single-portfolio.php
+++ archive-single-portfolio.php
@@ -4,17 +4,16 @@
     $thumbnailID = get_field( 'thumbnail-image' );
     $fullSizeID  = get_field( 'full-image' );
     $fullSizeURL = wp_get_attachment_url( $fullSizeID, 'full' );
-    $attrs       = array( 'class' => 'portfolio-thumbnail' );
 
     // If there's a custom Thumbnail sized image, we'll display that,
     // otherwise we'll fall back to the WordPress-built thumbnail
-    $image = ( !empty( $thumbnailID ) ) ?
-        wp_get_attachment_image( $thumbnailID, 'full', 0, $attrs ) :
-        wp_get_attachment_image( $fullSizeID, 'thumbnail', 0, $attrs );
+    $imageSRC = ( !empty( $thumbnailID ) ) ?
+        wp_get_attachment_image_src( $thumbnailID, 'full' ) :
+        wp_get_attachment_image_src( $fullSizeID, 'thumbnail' );
 
 ?>
 
 <figure class="portfolio-image">
     <figcaption><?php the_title(); ?></figcaption>
-    <a href="<?= $fullSizeURL ?>" class="portfolio-image-link"><?= $image ?></a>
+    <a href="<?= $fullSizeURL ?>" class="portfolio-image-link"><img src="<?= $imageSRC[0] ?>" alt="Thumbnail: <?= get_the_title() ?>" class="portfolio-thumbnail" /></a>
 </figure>
